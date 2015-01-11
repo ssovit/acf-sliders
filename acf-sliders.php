cb28ff6c32ff6c46dfab9ef5cda6982800dee785
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2015-01-11 12:39:16 +0545

    - Added support for ACF Pro v5+

--- acf-sliders.php
+++ acf-sliders.php
@@ -8,20 +8,27 @@ Version: 1.1
 Author: WPPress.net
 Author URI: http://wppress.net
 */
-if ( !defined( 'ABSPATH' )) die(-1); 
+if (!defined('ABSPATH')) {
+	/* Prevent Direct Access */
+	die(-1);
+}
 
 class WPPress_ACF_WPPress_Sliders
 {
-
+	
 	function __construct() {
-		add_action('acf/register_fields', array(
-			&$this,
+		add_action('acf/register_fields', array(&$this,
+			'register_fields'
+		));
+		
+		/* For ACF Pro 5+ */
+		add_action('acf/include_fields', array(&$this,
 			'register_fields'
 		));
 	}
-
+	
 	function register_fields() {
-		include_once (dirname(__FILE__).'/slider.class.php');
+		include_once (dirname(__FILE__) . '/slider.class.php');
 		if (class_exists('LS_Sliders')) {
 			include_once ('field-layerslider.php');
 		}
