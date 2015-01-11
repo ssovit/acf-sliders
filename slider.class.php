268471bbf4af9c6d7f7715c84fe44bea78a2d87b
Sovit Tamrakar <sovit.tamrakar@gmail.com>
2015-01-11 15:06:15 +0545

    Support for ACF Pro v5+

--- slider.class.php
+++ slider.class.php
@@ -120,7 +120,7 @@ abstract class WPPress_ACF_Field extends acf_field
 		));
 		echo '</td>';
 		echo '</tr>';
-		echo '<tr class="field_option field_option_'.$this->name . '">';
+		echo '<tr class="field_option field_option_' . $this->name . '">';
 		echo '<td class="label">';
 		echo '<label>' . __("Select Multiple?", 'acf') . '</label>';
 		echo '</td>';
@@ -154,6 +154,126 @@ abstract class WPPress_ACF_Field extends acf_field
 		));
 		echo '</td>';
 		echo '</tr>';
+	}
+	
+	function format_value($value, $field) {
+		
+		if (!$value || $value == 'null') {
+			return false;
+		}
+		if (is_array($value)) {
+			foreach ($value as $k => $v) {
+				$f = $this->slider_output($v);
+				$value[$k] = array();
+				$value[$k] = $f;
+			}
+		} else {
+			$value = $this->slider_output($value);
+		}
+		
+		return $value;
+	}
+	function render_field($field) {
+		$field['multiple'] = isset($field['multiple']) ? $field['multiple'] : false;
+		$field['disable'] = isset($field['disable']) ? $field['disable'] : false;
+		$field['hide_disabled'] = isset($field['hide_disabled']) ? $field['hide_disabled'] : false;
+		$multiple = '';
+		if ($field['multiple'] == '1') {
+			$multiple = ' multiple="multiple" size="5" ';
+			$field['name'].= '[]';
+		}
+		echo '<select id="' . $field['name'] . '" class="' . $field['class'] . '" name="' . $field['name'] . '" ' . $multiple . ' >';
+		if ($field['allow_null'] == '1') {
+			echo '<option value="null"> - Select - </option>';
+		}
+		$sliders = $this->slider_data();
+		if (!empty($sliders)) {
+			foreach ($sliders as $key => $value) {
+				$selected = '';
+				if (is_array($field['value'])) {
+					if (in_array($key, $field['value'])) {
+						$selected = 'selected="selected"';
+					}
+				} else {
+					if ($key == $field['value']) {
+						$selected = 'selected="selected"';
+					}
+				}
+				if (in_array(($key) , $field['disable'])) {
+					if ($field['hide_disabled'] == 0) {
+						echo '<option value="' . $key . '" ' . $selected . ' disabled="disabled">' . $value . '</option>';
+					}
+				} else {
+					echo '<option value="' . $key . '" ' . $selected . '>' . $value . '</option>';
+				}
+			}
+		}
+		
+		echo '</select>';
+	}
+	
+	function render_field_settings($field) {
+		$defaults = array(
+			'multiple' => 0,
+			'allow_null' => 0,
+			'default_value' => '',
+			'choices' => '',
+			'disable' => '',
+			'hide_disabled' => 0,
+		);
+		
+		$field = array_merge($defaults, $field);
+		$key = $field['name'];
+		
+		$sliders = $this->slider_data();
+		
+		$choices = array_merge(array(
+			0 => "---"
+		) , $sliders);
+		acf_render_field_setting($field, array(
+			'label' => __('Disabled Slides:', 'acf') ,
+			'type' => 'select',
+			'name' => 'fields[' . $key . '][disable]',
+			'value' => $field['disable'],
+			'multiple' => '1',
+			'allow_null' => '0',
+			'choices' => $choices,
+			'layout' => 'horizontal',
+		));
+		acf_render_field_setting($field, array(
+			'label' => __('Allow Null?', 'acf') ,
+			'type' => 'radio',
+			'name' => 'fields[' . $key . '][allow_null]',
+			'value' => $field['allow_null'],
+			'choices' => array(
+				1 => __("Yes", 'acf') ,
+				0 => __("No", 'acf') ,
+			) ,
+			'layout' => 'horizontal',
+		));
+		acf_render_field_setting($field, array(
+			'label' => __('Select Multiple?', 'acf') ,
+			'type' => 'radio',
+			'name' => 'fields[' . $key . '][multiple]',
+			'value' => $field['multiple'],
+			'choices' => array(
+				1 => __("Yes", 'acf') ,
+				0 => __("No", 'acf') ,
+			) ,
+			'layout' => 'horizontal',
+		));
+		acf_render_field_setting($field, array(
+			'label' => __('Hide disabled Slides?', 'acf') ,
+			
+			'type' => 'radio',
+			'name' => 'fields[' . $key . '][hide_disabled]',
+			'value' => $field['hide_disabled'],
+			'choices' => array(
+				1 => __("Yes", 'acf') ,
+				0 => __("No", 'acf') ,
+			) ,
+			'layout' => 'horizontal',
+		));
 	}
 }
 
